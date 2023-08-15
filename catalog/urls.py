88c877a05649f023e8f0ca2e1d36669b6f3a99a7
rusanov8 <rusanov.egor@bk.ru>
2023-08-16 01:03:06 +0700

    Update project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from catalog.views import get_home, get_contacts
+
+from catalog.apps import CatalogConfig
+from catalog.views import get_home, get_contacts, product_detail, create_product
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', get_home),
-    path('contacts/', get_contacts)
+    path('', get_home, name='home'),
+    path('contacts/', get_contacts, name='contacts'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('create_product/', create_product, name='create_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
 def get_home(request):
     """Контроллер, который отвечает за отображение домашней страницы"""
+    products_list = Product.objects.all()  # Список продуктов
 
-    # Выборка последних 5 товаров в консоль
-    latest_products = Product.objects.order_by('-create_date')[:5]
-    print('Последние 5 товаров:')
-    for product in latest_products:
-        print(product.title)
+    context = {
+        'object_list': products_list,
+        'title': 'Главная'
+    }
 
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/home.html', context)
 
 
 def get_contacts(request):
     """Контроллер, который отвечает за отображение контактной информации и обрабатывает POST запрос"""
+    context = {
+        'title': 'Контакты'
+    }
+
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
@@ -25,4 +30,35 @@ def get_contacts(request):
               f'Телефон: {phone}\n'
               f'Сообщение: {message}')
 
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product_detail(request, product_id):
+    """Контроллер для отображения страницы с товаром"""
+
+    product = Product.objects.get(pk=product_id)
+    context = {
+        'object': product,
+        'title': product.title
+    }
+    return render(request, 'catalog/product.html', context)
+
+
+def create_product(request):
+    """Контроллер для добавления нового товара"""
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:home')
+
+    else:
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'catalog/create_product.html', context)
+
+
